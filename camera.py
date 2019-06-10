17b71fc57d9c44f680649416a79b4785ef80dfac
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2019-06-10 13:02:19 -0400

    Camera shutdown code

--- camera.py
+++ camera.py
@@ -2,6 +2,7 @@ import time
 from threading import Thread
 import numpy as np
 import cv2
+import pyrealsense2 as rs
 
 obs_width = 0
 obs_height = 0
@@ -20,82 +21,53 @@ stop_thread = False
 def init(width=64, height=64):
     global obs_width
     global obs_height
-    global video_cap
-    global thread
+    global pipeline
 
     obs_width = width
     obs_height = height
 
-    # USB webcam
-    video_cap = cv2.VideoCapture(1)
+    config = rs.config()
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 60)
 
-    if not video_cap.isOpened():
-        raise Exception("Could not open video device")
+    pipeline = rs.pipeline()
 
-    video_cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    video_cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    print('starting pipeline')
+    profile = pipeline.start(config)
 
-    # Not yet implemented in OpenCV
-    #video_cap.set(cv2.CAP_PROP_BUFFERSIZE, 0)
+    #sensor = profile.get_device().query_sensors()[1]
 
-    stop_thread = False
+    #exp = sensor.get_option(rs.option.exposure)
+    #print('exposure:', exp)
 
-    thread = Thread(target = capture_thread, args=[])
-    thread.start()
+    #sensor.set_option(rs.option.enable_auto_exposure, 1)
+    #sensor.set_option(rs.option.enable_auto_white_balance, 1)
+
+    #sensor.set_option(rs.option.exposure, 50)
+    #sensor.set_option(rs.option.gamma, 100)
+    #sensor.set_option(rs.option.gain, 64)
+
+    #sensor.set_option(rs.option.saturation, 64)
+    #sensor.set_option(rs.option.sharpness, 30)
+    #sensor.set_option(rs.option.white_balance, 3200)
+    #sensor.set_option(rs.option.backlight_compensation, 0)
+
+    print('camera ready')
 
 def shutdown():
-    global video_cap
-    global stop_thread
-    stop_thread = True
-    thread.join()
-    video_cap.release()
-    video_cap = None
+    global pipeline
+    pipeline.stop()
 
 def get_image():
-    return last_img
+    global pipeline
 
-def capture_thread():
-    """
-    Thread to capture camera images continuously.
-    This is needed because OpenCV performs some amount of buffering
-    on captured images.
-    """
+    frames = pipeline.wait_for_frames()
+    img = frames.get_color_frame()
+    img = np.asanyarray(img.get_data())
 
-    global last_img
+    #print(img.dtype)
+    #print(img.shape)
 
-    while stop_thread != True:
-        last_img = _get_image()
-
-    print('Camera thread stopping')
-
-def _get_image():
-    """
-    Internal image read function used by capture thread. Do not call directly.
-    """
-
-    #print('reading image')
-
-    re, img = video_cap.read()
-
-    #print('img read')
-
-    if not re:
-        raise RuntimeError('Could not read image from camera.')
-
-    #print('Resizing image to {}x{}'.format(obs_width, obs_height))
-
-    # Drop some rows and columns to speed up resizing
-    img = img[::2, ::2]
-
-    # Resize to reduce the noise in the final image
-    # We resize locally to minimize network bandwidth required
-    #img = imresize(img, (obs_height, obs_width, 3), interp='cubic')
     img = cv2.resize(img, (obs_height, obs_width), interpolation=cv2.INTER_CUBIC)
-
-    #print('img resized')
-
-    # Convert image to RGB
-    img = img[...,::-1]
 
     img = np.ascontiguousarray(img, dtype=np.uint8)
 
